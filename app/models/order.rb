6cbf6a4fb40b85de440d674d868be0917452afd3
solacite <julia.nguyen.do@gmail.com>
2025-07-23 10:39:03 -0400

    fix double increment

--- app/models/order.rb
+++ app/models/order.rb
@@ -15,7 +15,7 @@ class Order < ApplicationRecord
     Rails.logger.debug "DEBUG: Product ID: #{product_id}, Current Inventory: #{product.inventory_count.inspect}, Quantity to decrease: #{quantity.inspect}"
 
     if product && product.inventory_count
-      product.decrement!(:inventory_count, quantity)
+      #product.decrement!(:inventory_count, quantity)
       Rails.logger.debug "DEBUG: Inventory updated! New inventory for Product #{product_id}: #{product.inventory_count}"
     else
       Rails.logger.error "ERROR: Product or inventory_count missing for Order #{self.id}. Product: #{product.inspect}"
