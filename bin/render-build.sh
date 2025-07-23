dea33481e49827e49ce0f6f78a93b7286a632a45
solacite <julia.nguyen.do@gmail.com>
2025-07-22 20:54:00 -0400

    clean render build, make new db

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,11 +6,19 @@ chmod +x bin/rails
 
 bundle install
 
-echo "--> Running database migrations"
+# --- NEW EXPLICIT DATABASE COMMANDS ---
+echo "--> Dropping existing database"
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bin/rails db:drop
+
+echo "--> Creating new database"
+bin/rails db:create
+
+echo "--> Running database migrations (explicitly before seeding)"
 bin/rails db:migrate
 
 echo "--> Running database seeds"
 bin/rails db:seed
+# --- END OF NEW EXPLICIT DATABASE COMMANDS ---
 
 echo "--> Precompiling assets"
 bin/rails assets:precompile
