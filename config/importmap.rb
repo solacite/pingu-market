d1b0da88a57d18bdceb401ab69856d403047a305
solacite <julia.nguyen.do@gmail.com>
2025-07-23 11:27:22 -0400

    fix active storage

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix" # @2.1.15
 pin "@rails/actiontext", to: "@rails--actiontext.js" # @8.0.200
+pin "@rails/activestorage", to: "activestorage.esm.js", preload: true
