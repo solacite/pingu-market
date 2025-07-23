a724d66bf7fd49989454e7de4a7801f689f25d6e
solacite <julia.nguyen.do@gmail.com>
2025-07-23 13:23:47 -0400

    add cloudinary debugging line

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,7 +1,10 @@
+Rails.logger.debug "DEBUG: Cloudinary initializer is being loaded."
+
 Cloudinary.config do |config|
   config.cloud_name = ENV['CLOUDINARY_CLOUD_NAME']
   config.api_key    = ENV['CLOUDINARY_API_KEY']
   config.api_secret = ENV['CLOUDINARY_API_SECRET']
-  # config.secure     = true # Often useful to force HTTPS URLs
-  # config.cdn_subdomain = true # If you want to use cdn.example.com for images
+  config.secure     = true
 end
+
+Rails.logger.debug "DEBUG: Cloudinary config after initializer: #{Cloudinary.config.cloud_name.present? ? 'Cloud name present' : 'Cloud name MISSING!'}"
