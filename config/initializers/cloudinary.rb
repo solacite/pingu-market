9b316bbd9da10287f3a4eee608370da7fd9b1220
solacite <julia.nguyen.do@gmail.com>
2025-07-23 13:31:33 -0400

    debug message to check if initializers load properly

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,10 +1,1 @@
-Rails.logger.debug "DEBUG: Cloudinary initializer is being loaded."
-
-Cloudinary.config do |config|
-  config.cloud_name = ENV['CLOUDINARY_CLOUD_NAME']
-  config.api_key    = ENV['CLOUDINARY_API_KEY']
-  config.api_secret = ENV['CLOUDINARY_API_SECRET']
-  config.secure     = true
-end
-
-Rails.logger.debug "DEBUG: Cloudinary config after initializer: #{Cloudinary.config.cloud_name.present? ? 'Cloud name present' : 'Cloud name MISSING!'}"
+Rails.logger.debug "DEBUG: Simple Cloudinary initializer test: The app started."
