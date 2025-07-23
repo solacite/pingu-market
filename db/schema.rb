743a037f29f2691631bc32a595861e2af09e8190
solacite <julia.nguyen.do@gmail.com>
2025-07-23 09:32:02 -0400

    add price column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_23_132325) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_23_133111) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -66,6 +66,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_23_132325) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "inventory_count"
+    t.decimal "price"
   end
 
   create_table "sessions", force: :cascade do |t|
