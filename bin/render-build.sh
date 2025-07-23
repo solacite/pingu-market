6d3a797e1b03620a08c14ff4126244f183212ca6
solacite <julia.nguyen.do@gmail.com>
2025-07-22 21:05:07 -0400

    made db use schema

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,19 +6,17 @@ chmod +x bin/rails
 
 bundle install
 
-# --- NEW EXPLICIT DATABASE COMMANDS ---
 echo "--> Dropping existing database"
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bin/rails db:drop
 
 echo "--> Creating new database"
 bin/rails db:create
 
-echo "--> Running database migrations (explicitly before seeding)"
-bin/rails db:migrate
+echo "--> Loading database schema from schema.rb"
+bin/rails db:schema:load
 
 echo "--> Running database seeds"
 bin/rails db:seed
-# --- END OF NEW EXPLICIT DATABASE COMMANDS ---
 
 echo "--> Precompiling assets"
 bin/rails assets:precompile
