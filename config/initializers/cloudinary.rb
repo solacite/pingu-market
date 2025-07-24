aa281d956f7d8353821ca4f9abf8d21355b1d0de
solacite <julia.nguyen.do@gmail.com>
2025-07-23 23:42:00 -0400

    fix active storage redirect issue

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -8,13 +8,15 @@ if ENV['CLOUDINARY_CLOUD_NAME'].present? && ENV['CLOUDINARY_API_KEY'].present? &
     config.api_secret = ENV['CLOUDINARY_API_SECRET']
     config.secure     = true
     config.url        = ENV['CLOUDINARY_URL'] if ENV['CLOUDINARY_URL'].present?
+
+    config.force_canonical_for_private_delivery = true 
   end
 
   Rails.logger.info "Cloudinary configured successfully from initializer."
   Rails.logger.info "Cloudinary.config.cloud_name: '#{Cloudinary.config.cloud_name}'"
   Rails.logger.info "Cloudinary.config.api_key present: #{Cloudinary.config.api_key.present?}"
   Rails.logger.info "Cloudinary.config.secure: #{Cloudinary.config.secure}"
-  Rails.logger.info "Cloudinary.config.url: '#{Cloudinary.config.url}'"
+  Rails.logger.info "Cloudinary.config.url: '#{Cloudinary.config.url}'" 
 else
   Rails.logger.warn "WARNING: Cloudinary environment variables are not fully set in initializer. Check CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, CLOUDINARY_URL."
   Rails.logger.warn "CLOUDINARY_CLOUD_NAME: '#{ENV['CLOUDINARY_CLOUD_NAME']}'"
