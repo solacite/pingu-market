030510a6cef2280f5b305642f6e492a73be9db27
solacite <julia.nguyen.do@gmail.com>
2025-07-23 23:30:03 -0400

    check if cloudinary config loads properly

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,4 +1,7 @@
 if ENV['CLOUDINARY_CLOUD_NAME'].present? && ENV['CLOUDINARY_API_KEY'].present? && ENV['CLOUDINARY_API_SECRET'].present?
+
+  Rails.logger.info "Cloudinary raw ENV['CLOUDINARY_URL']: '#{ENV['CLOUDINARY_URL']}'"
+
   Cloudinary.config do |config|
     config.cloud_name = ENV['CLOUDINARY_CLOUD_NAME']
     config.api_key    = ENV['CLOUDINARY_API_KEY']
@@ -6,9 +9,12 @@ if ENV['CLOUDINARY_CLOUD_NAME'].present? && ENV['CLOUDINARY_API_KEY'].present? &
     config.secure     = true
     config.url        = ENV['CLOUDINARY_URL'] if ENV['CLOUDINARY_URL'].present?
   end
+
   Rails.logger.info "Cloudinary configured successfully from initializer."
   Rails.logger.info "Cloudinary.config.cloud_name: '#{Cloudinary.config.cloud_name}'"
   Rails.logger.info "Cloudinary.config.api_key present: #{Cloudinary.config.api_key.present?}"
+  Rails.logger.info "Cloudinary.config.secure: #{Cloudinary.config.secure}"
+  **Rails.logger.info "Cloudinary.config.url: '#{Cloudinary.config.url}'"
 else
   Rails.logger.warn "WARNING: Cloudinary environment variables are not fully set in initializer. Check CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, CLOUDINARY_URL."
   Rails.logger.warn "CLOUDINARY_CLOUD_NAME: '#{ENV['CLOUDINARY_CLOUD_NAME']}'"
